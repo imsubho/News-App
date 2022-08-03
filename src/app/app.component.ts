d41f5c085d3f7657d846a9f07f9cb77fd0f29461
SUBHADEEP MITRA <imsubho>
2022-08-03 22:08:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { NewsService } from './service/news.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'NewsApp';
+export class AppComponent implements AfterViewInit  {
+  title = 'NewsProject';
+  sources: any = [];
+  articles:any = [];
+  selectedNewsChannel: string="Top 10 Trending News!";
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+  ngOnInit(): void {
+    this.newsApi.initArticles().subscribe((res:any)=>{
+      console.log(res);
+      this.articles = res.articles;
+    })
+    this.newsApi.initSources().subscribe((res:any)=>{
+      console.log(res);
+      this.sources = res.sources;
+    })
+
+  }
+  constructor(private observer : BreakpointObserver, private cdr : ChangeDetectorRef, private newsApi : NewsService){
+
+  }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+    .subscribe((res)=>{
+      if(res?.matches){
+        this.sideNav.mode="over";
+        this.sideNav.close();
+      }else{
+        this.sideNav.mode = 'side';
+        this.sideNav.open();
+      }
+    })
+    this.cdr.detectChanges();
+  }
+  searchSource(source:any){
+    this.newsApi.getArticlesByID(source.id)
+    .subscribe((res:any)=>{
+      this.articles = res.articles;
+      this.selectedNewsChannel = source.name
+      
+    })
+  }
 }
---
+++ src/app/service/news.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  api_key = 'c26587279dad4b36b200d93755aa496b';
+  constructor(private http: HttpClient) { }
+
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+
+}
+
